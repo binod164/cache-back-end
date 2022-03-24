5258227f53dd67ee63bca48fd0d3fdab2ac55525
charlesjbasedow <cbasedow@outlook.com>
2022-03-24 15:21:57 -0400

    add income model

--- models/profile.js
+++ models/profile.js
@@ -5,8 +5,9 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
-},{
-    timestamps: true,
+  income: [incomeSchema],
+}, {
+  timestamps: true
 })
 
 const incomeSchema = new Schema({
@@ -20,6 +21,7 @@ const incomeSchema = new Schema({
   },
   date: {
     type: Date,
+    required: true,
     default: function(){
       let date = new Date()
       return date.setDate(date.getDate())
