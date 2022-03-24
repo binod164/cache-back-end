8d558d8e4a7c74241354a6aaa5055a5777d67474
charlesjbasedow <cbasedow@outlook.com>
2022-03-24 15:29:56 -0400

    add expense model

--- models/profile.js
+++ models/profile.js
@@ -6,12 +6,31 @@ const profileSchema = new Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
   income: [incomeSchema],
+  expense: [expenseSchema],
 }, {
   timestamps: true
 })
 
 const incomeSchema = new Schema({
   name: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: function(){
+      let date = new Date()
+      return date.setDate(date.getDate())
+  }}
+})
+
+const expenseSchema = new Schema({
+  category: {
     type: String,
     required: true
   },
