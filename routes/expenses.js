1852ee325c7033ba43c35d8b7c267df213eca9c3
charlesjbasedow <cbasedow@outlook.com>
2022-03-29 10:41:29 -0400

    clean up backend

--- routes/expenses.js
+++ routes/expenses.js
@@ -5,14 +5,11 @@ import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
-
-
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, expensesCtrl.create)
 router.get('/', checkAuth, expensesCtrl.index)
 router.delete('/:id', checkAuth, expensesCtrl.delete)
-
 
 export {
   router
